dad93e5350dfd943e8df28147435eaf171d6027f
Michal Todt <todt.michal@gmail.com>
2023-08-08 04:22:39 +0200

    buxfix insert to the database

--- sreality/sreality/spiders/sreality_spider.py
+++ sreality/sreality/spiders/sreality_spider.py
@@ -1,10 +1,32 @@
-import time
-
-from ..db import set_up_db, insert_ad, get_connection
-
 import scrapy
 import json
 import unicodedata
+
+import psycopg2
+
+
+def get_connection():
+    return psycopg2.connect("dbname='postgres' user='postgres' host='db' password='password'")
+
+
+def set_up_db(connection):
+    with connection.cursor() as c:
+        c.execute("""
+        CREATE TABLE IF NOT EXISTS sreality (
+            ad_name VARCHAR (255),
+            img_url TEXT );
+        """)
+
+
+def insert_ad(ad_name, image_url, connection):
+    with connection.cursor() as c:
+        c.execute(f"INSERT INTO sreality (ad_name, img_url) VALUES ('{ad_name}', '{image_url}');")
+
+
+def read_ads(connection):
+    with connection.cursor() as c:
+        c.execute("SELECT * FROM sreality;")
+        return c.fetchall()
 
 
 class SrealitySpider(scrapy.Spider):
